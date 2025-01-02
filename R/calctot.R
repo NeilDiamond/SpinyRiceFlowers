e230ab1b3c564740fb425faa4fa77449f8f18b2e
neil.diamond@esquant.com.au <neil.diamond@esquant.com.au>
2025-01-02 15:14:06 +1100

    dontrun example in calctot()

--- R/calctot.R
+++ R/calctot.R
@@ -9,6 +9,7 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' ff <- ks::kde.boundary(AllPlantsby2020[,2:3],
 #'                     xmin=c(0,0),xmax=c(35,15))
 #' probs1 <- quadratprobs(ff, quadratstart=c(0,0),quadratsize=5)
@@ -19,6 +20,7 @@
 #' results
 #' Hmisc::wtd.mean(as.numeric(names(results)), results)
 #' sqrt(Hmisc::wtd.var(as.numeric(names(results)), results))
+#' }
 calctot <- function(ss, probs, incprobs, qx){
   sval <- wrswoR::sample_int_crank(prod(sapply(attributes(qx)$dimnames,length)),
                                                ss, prob=probs)
