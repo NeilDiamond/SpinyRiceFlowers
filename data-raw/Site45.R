1fe7d21066b02ad7b23066bd0f723267917d7d97
neil.diamond@esquant.com.au <neil.diamond@esquant.com.au>
2025-01-03 14:30:39 +1100

    Added underscores to make file portable

--- data-raw/Site45.R
+++ data-raw/Site45.R
@@ -1,5 +1,5 @@
 ## code to prepare `Site45` dataset goes here
-Site45 <- readxl::read_excel("./inst/extdata/Site 45 Phylands Pimelea count.xlsx")
+Site45 <- readxl::read_excel("./inst/extdata/Site_45_Phylands_Pimelea_count.xlsx")
 
 
 rangex <- range(c(Site45$left))
@@ -9,7 +9,7 @@ Site45$y <- Site45$bottom+0.5-rangey[1]
 Site45 <- Site45[,c("id_2","x","y","NUMPimelea")]
 Site45 <- Site45[Site45$NUMPimelea>0,]
 
-Site45 <- read.csv("./inst/extdata/Site 45 WGS84 convert UTM.csv")
+Site45 <- read.csv("./inst/extdata/Site_45_WGS84_convert_UTM.csv")
 Site45 <- data.frame(Tag_ID=1:nrow(Site45), x.axis=Site45[,"east"],
                      y.axis=Site45[,"north"])
 Site45$x.axis <- Site45$x.axis-min(Site45$x.axis)
@@ -18,8 +18,7 @@ colnames(Site45) <- c("Tag ID", "x-axis", "y-axis")
 
 usethis::use_data(Site45, overwrite = TRUE)
 
-plot(Site45[,2:3])
-apply(Site45,2, range)
 
 
-usethis::use_data(Site45, overwrite = TRUE)
+
+
