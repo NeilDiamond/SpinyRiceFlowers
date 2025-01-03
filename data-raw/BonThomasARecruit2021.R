67adaa7211f07eb729a052f49f56f6d1f2525fab
neil.diamond@esquant.com.au <neil.diamond@esquant.com.au>
2025-01-03 14:27:37 +1100

    Changed x-axis position to x-axis, sim for y

--- data-raw/BonThomasARecruit2021.R
+++ data-raw/BonThomasARecruit2021.R
@@ -3,7 +3,7 @@
 BonThomasARecruit2021 <-
   BonThomasA[BonThomasA$`Status 2020`=="Germinant" &
              BonThomasA$`Status 2021`=="Alive",
-             c("Tag ID", "x-axis position", "y-axis position")]
+             c("Tag ID", "x-axis", "y-axis")]
 
 
 usethis::use_data(BonThomasARecruit2021, overwrite = TRUE)
