caec664a83b79c240b552813e738514df22911ac
neil.diamond@esquant.com.au <neil.diamond@esquant.com.au>
2025-01-03 14:29:14 +1100

    Added underscores to make file portable

--- data-raw/Site29.R
+++ data-raw/Site29.R
@@ -1,7 +1,7 @@
 ## code to prepare `Site29` dataset goes here
 
 
-Site29 <- readxl::read_excel("./inst/extdata/Site 29 Jasper Road Patch220 count.xlsx")
+Site29 <- readxl::read_excel("./inst/extdata/Site_29_Jasper_Road_Patch220_count.xlsx")
 
 rangex <- range(c(Site29$left))
 rangey <- range(c(Site29$bottom))
